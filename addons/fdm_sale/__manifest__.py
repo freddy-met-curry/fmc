a5edc8a41d1d54fafd64fcdd06b3ede194c3fbad
asma.elferkhsi <asma.elferkhsi@eezee-it.com>
2022-10-13 11:53:24 +0100

    update

---
+++ addons/fdm_sale/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Eezee-IT (<http://www.eezee-it.com>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Freddymetcurry Sale',
+    'version': '15.0.1.0.0',
+    'author': 'Eezee-It',
+    'website': 'http://www.eezee-it.com',
+    'category': 'Sales',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale',
+
+    ],
+    'data': [
+        'views/sale_views.xml'
+    ],
+    'installable': True,
+}
