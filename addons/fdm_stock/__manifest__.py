d31c2ab6db841419b98835e1f0100bc307d4f82a
asma.elferkhsi <asma.elferkhsi@eezee-it.com>
2022-10-13 11:14:45 +0100

    customer

--- addons/fdm_stock/__manifest__.py
+++ addons/fdm_stock/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Freddymetcurry Stock',
-    'version': '15.0.1.0.0',
+    'version': '15.0.1.0.2',
     'author': 'Eezee-It',
     'website': 'http://www.eezee-it.com',
     'category': 'Inventory',
@@ -14,7 +14,7 @@
         'calendar',
     ],
     'data': [
-
+        'views/stock_picking.xml'
     ],
     'assets': {
         'web.assets_backend': [
