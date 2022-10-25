9f9156340c03ff97ad6d47fced61287c61e8c472
asma.elferkhsi <asma.elferkhsi@eezee-it.com>
2022-10-25 12:28:57 +0100

    invoice

---
+++ addons/fdm_account/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Eezee-IT (<http://www.eezee-it.com>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Freddymetcurry Account',
+    'version': '15.0.1.0.0',
+    'author': 'Eezee-It',
+    'website': 'http://www.eezee-it.com',
+    'category': 'Account',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+        'account',
+    ],
+    'data': [
+        'report/report_invoice.xml',
+    ],
+    'installable': True,
+}
