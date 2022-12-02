30660b26740b6934e17ef01018edc7da98324fb8
asma.elferkhsi <asma.elferkhsi@eezee-it.com>
2022-12-02 14:01:10 +0100

    change version

--- addons/fdm_account/__manifest__.py
+++ addons/fdm_account/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Freddymetcurry Account',
-    'version': '15.0.1.0.0',
+    'version': '15.0.1.0.1',
     'author': 'Eezee-It',
     'website': 'http://www.eezee-it.com',
     'category': 'Account',
