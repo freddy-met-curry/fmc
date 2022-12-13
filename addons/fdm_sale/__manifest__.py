6a32d67bdc43fd2e5d6febc18758ba6330c642e8
Asma Elferkhsi <asma.elferkhsi@eezee-it.com>
2022-12-13 09:24:07 +0000

    [ase-fdm-26288]  Restauration description devis et factures

--- addons/fdm_sale/__manifest__.py
+++ addons/fdm_sale/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Freddymetcurry Sale',
-    'version': '15.0.1.0.1',
+    'version': '15.0.1.0.3',
     'author': 'Eezee-It',
     'website': 'http://www.eezee-it.com',
     'category': 'Sales',
@@ -13,8 +13,8 @@
 
     ],
     'data': [
-        'report/report_sale_order.xml',
         'views/sale_views.xml',
+        'report/report_sale_order.xml',
     ],
     'installable': True,
 }
--- addons/fdm_sale/models/sale_order.py
+++ addons/fdm_sale/models/sale_order.py
@@ -1,95 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models
-from odoo.tools import float_compare
+from odoo import models, fields
 
 
-class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
 
-    def _action_launch_stock_rule(self, previous_product_uom_qty=False):
-        if self._context.get("skip_procurement"):
-            return True
-        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
-        procurements = []
-        for line in self:
-            line = line.with_company(line.company_id)
-            if line.state != 'sale' or line.product_id.type not in ('consu', 'product'):
-                continue
-            qty = line._get_qty_procurement(previous_product_uom_qty)
-            if float_compare(qty, line.product_uom_qty, precision_digits=precision) == 0:
-                continue
+    nbre_pax = fields.Char()
+    nbre_staff = fields.Char()
+    notes_comp = fields.Char('Additional notes')
 
-            group_id = line._get_procurement_group()
-            if not group_id:
-                group_id = self.env['procurement.group'].create(line._prepare_procurement_group_vals())
-                line.order_id.procurement_group_id = group_id
-            else:
-                # In case the procurement group is already created and the order was
-                # cancelled, we need to update certain values of the group.
-                updated_vals = {}
-                if group_id.partner_id != line.order_id.partner_shipping_id:
-                    updated_vals.update({'partner_id': line.order_id.partner_shipping_id.id})
-                if group_id.move_type != line.order_id.picking_policy:
-                    updated_vals.update({'move_type': line.order_id.picking_policy})
-                if updated_vals:
-                    group_id.write(updated_vals)
+    def write(self, values):
+        res = super(SaleOrder, self).write(values)
+        for record in self:
+            if values.get('nbre_pax'):
+                record.picking_ids.write({'nbre_pax': record.nbre_pax})
+            if values.get('nbre_staff'):
+                record.picking_ids.write({'nbre_staff': record.nbre_staff})
+            if values.get('notes_comp'):
+                record.picking_ids.write({'notes_comp': record.notes_comp})
 
-            values = line._prepare_procurement_values(group_id=group_id)
-            product_qty = line.product_uom_qty - qty
+        return res
 
-            line_uom = line.product_uom
-            quant_uom = line.product_id.uom_id
-            product_qty, procurement_uom = line_uom._adjust_uom_quantities(product_qty, quant_uom)
-            if group_id and group_id.stock_move_ids:
-                product_ids = group_id.stock_move_ids.mapped('product_id')
-                if line.product_id in product_ids:
-                    move_id = group_id.stock_move_ids.filtered(lambda m: m.product_id.id == line.product_id.id)
-                    if move_id and len(move_id) == 1:
-                        product_uom_qty = move_id.product_uom_qty + product_qty
-                        move_id.write({'product_uom_qty': product_uom_qty})
-                    else:
-                        procurements.append(self.env['procurement.group'].Procurement(
-                            line.product_id, product_qty, procurement_uom,
-                            line.order_id.partner_shipping_id.property_stock_customer,
-                            line.name, line.order_id.name, line.order_id.company_id, values))
-                else:
-                    procurements.append(self.env['procurement.group'].Procurement(
-                        line.product_id, product_qty, procurement_uom,
-                        line.order_id.partner_shipping_id.property_stock_customer,
-                        line.name, line.order_id.name, line.order_id.company_id, values))
-            else:
-                product_ids = [p.product_id.id for p in procurements]
-                if line.product_id.id in product_ids:
-                    i = 0
-                    for p in procurements:
-                        if p.product_id == line.product_id and p.product_uom == line.product_uom:
-                            new_qty = p.product_qty + product_qty
-                            new_p = self.env['procurement.group'].Procurement(
-                                line.product_id, new_qty, procurement_uom,
-                                line.order_id.partner_shipping_id.property_stock_customer,
-                                line.name, line.order_id.name, line.order_id.company_id, values)
-                            procurements.pop(i)
-                            procurements.append(new_p)
-                            i += 1
-                            break
-                        else:
-                            i += 1
-                else:
-                    procurements.append(self.env['procurement.group'].Procurement(
-                        line.product_id, product_qty, procurement_uom,
-                        line.order_id.partner_shipping_id.property_stock_customer,
-                        line.name, line.order_id.name, line.order_id.company_id, values))
-        if procurements:
-            self.env['procurement.group'].run(procurements)
-
-        # This next block is currently needed only because the
-        # scheduler trigger is done by picking confirmation
-        # rather than stock.move confirmation
-        orders = self.mapped('order_id')
-        for order in orders:
-            pickings_to_confirm = order.picking_ids.filtered(lambda p: p.state not in ['cancel', 'done'])
-            if pickings_to_confirm:
-                # Trigger the Scheduler for Pickings
-                pickings_to_confirm.action_confirm()
-        return True
+    def _action_confirm(self):
+        res = super(SaleOrder, self)._action_confirm()
+        self.picking_ids.write({'nbre_pax': self.nbre_pax,
+                                'nbre_staff': self.nbre_staff,
+                                'notes_comp': self.notes_comp,
+                                })
+        return res
