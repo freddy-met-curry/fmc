5eae7266d036d96eb58c6900aec480cc76b00419
asma.elferkhsi <asma.elferkhsi@eezee-it.com>
2022-12-22 12:53:38 +0100

    invoice

--- addons/fdm_sale/__manifest__.py
+++ addons/fdm_sale/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Freddymetcurry Sale',
-    'version': '15.0.1.0.3',
+    'version': '15.0.1.0.4',
     'author': 'Eezee-It',
     'website': 'http://www.eezee-it.com',
     'category': 'Sales',
