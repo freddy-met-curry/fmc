febc4f3ba3f0e5d3084af3823decb9085d07678a
asma.elferkhsi <asma.elferkhsi@eezee-it.com>
2023-04-04 12:05:10 +0100

    update label

--- addons/fdm_mrp/models/product_template.py
+++ addons/fdm_mrp/models/product_template.py
@@ -6,4 +6,7 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    is_menu = fields.Boolean('Menu')
+    is_menu = fields.Boolean('Details of the articles to be produced',
+                             help="This field allows you to detail the items of the nomenclature"
+                                  " to be produced for the products to "
+                                  "be delivered to the customer on the production sheet")
