3ff5e5382e6ad8f78f046df7a24bb61244c44441
RobinFreddy <robin@freddymetcurry.be>
2024-02-19 21:16:41 +0100

    Delete addons

--- addons/fdm_sale/wizard/base_document_layout.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class BaseDocumentLayout(models.TransientModel):
-    _inherit = 'base.document.layout'
-
-    logo2 = fields.Binary(related='company_id.logo2')
--- addons/fdm_website/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    min_delay = fields.Datetime(string='Web order date')
